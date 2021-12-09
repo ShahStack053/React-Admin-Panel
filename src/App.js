6be57e80b38fe9ca699315fc8dd7d3cc4350c831
raza <shahhsoft@gmail.com>
2021-12-09 12:26:04 +0500

    switch import set

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import Sidebar from "./Components/sidebar/Sidebar";
 import Topbar from "./Components/topbar/Topbar";
 import "./app.css";
 import Home from "./Components/pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import UserList from "./Components/pages/userList/UserList";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
       <div className="container">
         <Sidebar />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/users">
